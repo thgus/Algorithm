9c9fa603b17da3ea3252bd53e0f043c47799653b
sohyeon <t01081418141@naver.com>
2022-03-21 00:26:26 +0900

    Update baekjoon 11062.cpp annotation

--- cpp/DP/11062.cpp
+++ cpp/DP/11062.cpp
@@ -18,7 +18,7 @@ void solve(int n)
 {
 	dp.clear();
 	dp.resize(n, vector<int>(n, -1));
-	dfs1(0, n - 1);
+	cout << dfs1(0, n - 1) << "\n";
 }
 
 int main()
@@ -34,40 +34,35 @@ int main()
 		for (int i = 0; i < n; i++)	cin >> vec[i];
 
 		solve(n);
-
-		cout << dp[0][n - 1] << "\n";
 	}
 
 }
 
-// 근우. 근우는 큰 값을 고르도록 유도
+// 근우차례
+// 근우는 자신이 선택한 값이 최대가 되도록 한다
 int dfs1(int left, int right)
 {
-	if (left == right)
-	{
-		dp[left][right] = vec[left];
-		return dp[left][right];
-	}
-	//if (left > right)	return 0;
-
 	if (dp[left][right] != -1)	return dp[left][right];
+	if (left == right)	return vec[left];
 
 	dp[left][right] = max(vec[left]+dfs2(left + 1, right), vec[right]+dfs2(left, right - 1));
 
 	return dp[left][right];
 }
 
-// 명우. 명우 차례에 명우가 더 작은 수를 선택하도록 유도
+// 명우
+// 명우 입장에서는 근우가 더 작은 수를 만들도록 할 것임
 int dfs2(int left, int right)
 {
-	//if (left > right)	return 0;
-	if (left == right)
-	{
-		dp[left][right] = 0;
-		return 0;
-	}
 	if (dp[left][right] != -1)	return dp[left][right];
+	if (left == right)	return 0;
 
+
+	// (3,2,1) 이 구간에서 가질 수 있는 값은 총 6으로 정해져 있음
+	// 왼쪽을 고르면 근우가 갖는 예상 값은 2이고 명우는 무조건 4를 얻을 수 있음
+	// 오른쪽을 고르면 근우가 갖는 예상 값은 3이고 명우는 무조건 3을 얻을 수 있음
+	// 명우가 더 높은 수를 얻으려면 근우가 더 낮은 수를 얻도록 만들어야 함.
+	// 따라서 위의 경우에서는 명우는 왼쪽을 선택할 것이다.
 	dp[left][right] = min(dfs1(left + 1, right), dfs1(left, right - 1));
 
 	return dp[left][right];
